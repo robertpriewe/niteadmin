78049bd18ada50cf2eb0f0ccf594f7db36c82b6b
robert <robertpriewe@gmail.com>
2020-08-19 23:03:23 -0400

    layout fixes

--- modules/header.php
+++ modules/header.php
@@ -18,18 +18,20 @@
     <link href="assets/libs/custombox/custombox.min.css" rel="stylesheet">
 
     <!-- App css -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
-    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
 
     <link href="assets/css/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
     <link href="assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
+
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
 
     <!-- icons -->
 
     <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body class="" data-sidebar-color="dark" data-layout='{"sidebar": { "color": "dark", "size": "default", "showuser": false}, "topbar": {"color": "brand"} }'>
+<body class="" data-layout='{"sidebar": { "color": "dark", "size": "default", "showuser": false}, "topbar": {"color": "brand"} }'>
 
 <!-- Begin page -->
 <div id="wrapper">

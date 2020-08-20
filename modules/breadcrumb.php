6d6a9943ac37953039f02bff10766a64d9b7c8c8
robert <robertpriewe@gmail.com>
2020-08-19 23:03:31 -0400

    event / shows links

--- modules/breadcrumb.php
+++ modules/breadcrumb.php
@@ -2,10 +2,21 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box">
-            <h4 class="page-title"><?php echo $title; ?></h4>
+            <h4 class="page-title"><?php
+                echo $title;
+                if (isset($eventname)) {
+                    echo ' for Event: ' . $eventname;
+                }
+                ?></h4>
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item"><a href="?">Home</a></li>
+                    <?php
+                    if (isset($eventid)) {
+                        echo '<li class="breadcrumb-item"><a href="?page=showevents">Event List</a></li>';
+                        echo '<li class="breadcrumb-item"><a href="?page=eventdetails&eventid=' . $eventid . '">Event Details</a></li>';
+                    }
+                    ?>
                     <li class="breadcrumb-item active"><?php echo $title; ?></li>
                 </ol>
             </div>

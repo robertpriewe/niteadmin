fe44260cba0fd33584adbe2580ec4ff536b81516
robert <robertpriewe@gmail.com>
2020-09-24 21:29:40 -0400

    title

--- modules/header.php
+++ modules/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Nite Admin</title>
+    <title>Nite Admin - <?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Nite Admin Venue Management & Nightlife System." name="description" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />

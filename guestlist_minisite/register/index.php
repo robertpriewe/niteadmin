a2be2c1a2a82d1ce0da7e29fb17ba0a9ed669211
robert <robertpriewe@gmail.com>
2020-11-04 12:46:08 -0500

    edit guestlist spots

--- guestlist_minisite/register/index.php
+++ guestlist_minisite/register/index.php
@@ -1,6 +1,34 @@
 <?php
+if (!isset($_GET['link'])) {
+    echo "Link is missing the registration hash";
+    die;
+}
 include('sql.php');
-$query = mysqli_query($mysqli, "SELECT * FROM guestlist LEFT JOIN guestlist_access ON guestlist.ACCESS = guestlist_access.ACCESSID LEFT JOIN events ON guestlist.EVENTID = events.EVENTID WHERE GROUPHASH = '" . $_GET['link'] . "' ORDER BY FIRSTNAME, LASTNAME ASC");
+
+if (isset($_GET['action']) && isset($_POST)) {
+    $updateid = [];
+    $idcount = 0;
+    foreach($_POST as $key => $val) {
+        if ($val <> "") {
+            $fieldid = explode("-", $key)[1];
+            $fieldname = explode("-", $key)[0];
+            $updateid[$idcount] = $fieldid;
+            $idcount++;
+        }
+    }
+
+    $group = array();
+    foreach ( $updateid as $value ) {
+        $group[$value][] = $value;
+    }
+
+    foreach ($group as $key => $value) {
+        mysqli_query($mysqli, "UPDATE guestlist SET FIRSTNAMEGUEST = '" . $_POST['firstName-' . $key] . "', LASTNAMEGUEST = '" . $_POST['lastName-' . $key] . "', EMAILGUEST = '" . $_POST['email-' . $key] . "' WHERE ID = " . $key . " AND GROUPHASH = '" . $_GET['link'] . "'");
+    }
+    $saved = "true";
+}
+
+$query = mysqli_query($mysqli, "SELECT * FROM guestlist LEFT JOIN guestlist_access ON guestlist.ACCESS = guestlist_access.ACCESSID LEFT JOIN events ON guestlist.EVENTID = events.EVENTID WHERE GROUPHASH = '" . $_GET['link'] . "' ORDER BY ACCESSLEVEL, LASTNAME, FIRSTNAME ASC");
 
 if ($query->num_rows > 0) {
     while ($row = $query->fetch_assoc()) {
@@ -12,11 +40,14 @@ if ($query->num_rows > 0) {
         $checkin[] = $row['CHECKIN'];
         $eventname = $row['EVENTNAME'];
         $eventstart = $row['EVENTSTARTDATE'];
+        $rowscol[] = $row;
     }
 } else {
     echo "Your link is damaged or invalid, please contact the event organizer if you believe this is an error";
     die;
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,38 +71,120 @@ if ($query->num_rows > 0) {
 
 </head>
 
-<body>
-
 <div>
-    <div class="col-12">
-        <div class="row">
-            <div class="col-12">
-                <div class="page-title-box">
-                    <div class="page-title-right">
-                        <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Home</a></li>
-                            <li class="breadcrumb-item active">Guestlist</li>
-                        </ol>
+    <?php
+    $accessvar = "";
+    foreach($rowscol as $rowresult) {
+        $accesscount[$rowresult['ACCESSLEVEL']] = 0;
+    }
+    foreach($rowscol as $rowresult) {
+        $accesscount[$rowresult['ACCESSLEVEL']]++;
+    }
+
+
+
+    ?>
+
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box">
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Home</a></li>
+                                <li class="breadcrumb-item active">Guestlist</li>
+                            </ol>
+                        </div>
+                        <h4 class="page-title">Guestlist for: <?php echo $eventname; ?></h4>
                     </div>
-                    <h4 class="page-title">Guestlist for: <?php echo $eventname; ?></h4>
                 </div>
             </div>
         </div>
-    </div>
+    <?php
+    if (isset($saved)) {
+        echo ' <div class="col-12">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        Changes have been saved!
+                    </div>
+                </div>
+            </div>
+        </div>';
+    }
+    ?>
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-secondary" role="alert">
+                        Names can be changed until the start of the event!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <form action="?link=<?php echo $_GET['link']; ?>&action=submit" method="POST">
+                    <?php
+                    foreach($rowscol as $rowresult) {
+                        if ($accessvar <> $rowresult['ACCESSLEVEL']) {
+                            if ($accessvar <> "") {
+                                echo '
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+                            }
+                            echo '
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card ribbon-box">
+                            <div class="card-body">
+                                <div class="ribbon ribbon-purple float-left"><i class="mdi mdi-access-point mr-1"></i> ' . $rowresult['ACCESSLEVEL'] . '</div>
+                                <h5 class="text-purple float-right mt-0">' . $rowresult['ACCESSLEVEL'] . ' Slots: ' . $accesscount[$rowresult['ACCESSLEVEL']] . '</h5>
+                                <div class="ribbon-content">
+                                    <div class="form-row">';
+                            $accessvar = $rowresult['ACCESSLEVEL'];
+                        }
+                        echo '<div class="form-group col-4">
+                                            <label class="col-form-label">First Name</label>
+                                            <input type="text" class="form-control" name="firstName-' . $rowresult['ID'] . '" placeholder="First Name" value="' . $rowresult['FIRSTNAMEGUEST'] . '">
+                                        </div>
+                                        <div class="form-group col-4">
+                                            <label class="col-form-label">Last Name</label>
+                                            <input type="text" class="form-control" name="lastName-' . $rowresult['ID'] . '" placeholder="Last Name" value="' . $rowresult['LASTNAMEGUEST'] . '">
+                                        </div>
+                                        <div class="form-group col-4">
+                                            <label class="col-form-label">E-Mail</label>
+                                            <input type="email" class="form-control" name="email-' . $rowresult['ID'] . '" placeholder="E-Mail" value="' . $rowresult['EMAILGUEST'] . '">
+                                        </div>';
+                    }
+                    ?>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+
+            </form>
+        </div>
 
     <div class="col-12">
         <div class="row">
             <div class="col-12">
-                <div class="card-box">
-                    <div class="col-3"></div>
-                </div>
+                &nbsp;
             </div>
         </div>
     </div>
-
-    <!-- ============================================================== -->
-    <!-- End Page content -->
-    <!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- End Page content -->
+<!-- ============================================================== -->
 
 </div>
 
